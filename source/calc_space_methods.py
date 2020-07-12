d61bc1ec0c9a1919c20ec0c3ce1c1f90b42e6640
Hossam Amer <h2amer@multicom-pc52.eng.uwaterloo.ca>
2020-07-12 17:29:32 -0400

    added some interface methods for calc space methods

--- source/calc_space_methods.py
+++ source/calc_space_methods.py
@@ -1,2 +1,45 @@
 # This file should contain all equations for calculating the space
 
+# Force floating point division
+from __future__ import division
+import math
+
+# Import conv layer
+from conv_layer import Conv_Layer
+
+# Calculates the required memory units for the **CPO** method
+# is_for_density is a flag to know whether we should use the assumptions for space calculations or not
+def getSpaceCPO(is_for_density_calc=True):
+
+    if is_for_density_calc:
+        space = 0
+    else:
+        space = 1
+    return space
+
+# Calculates the required memory units for the **MEC**  method
+# is_for_density is a flag to know whether we should use the assumptions for space calculations or not
+def getSpaceMEC(is_for_density_calc=True):
+
+    if is_for_density_calc:
+        space = 0
+    else:
+        space = 1
+    return space
+
+# Calculates the required memory units for the **CSCC** method
+# is_for_density is a flag to know whether we should use the assumptions for space calculations or not
+def getSpaceCSCC(is_for_density_calc=True):
+    
+    if is_for_density_calc:
+        space = 0
+    else:
+        space = 1
+    return space
+
+# Calculates the required memory units for the **Im2Col** method
+def getSpaceIm2Col():
+    space = 0
+    return space
+
+
