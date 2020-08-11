62306ecc54b67e771d30414860d59185946bba0d
Hossam Amer <h2amer@multicom-pc52.eng.uwaterloo.ca>
2020-08-11 17:06:37 -0400

    wrote script to run everything

---
+++ source/do_all_models.sh
@@ -0,0 +1,9 @@
+time /usr/bin/python3 run_inference.py --select Org --END 2 --model_name IV1
+time /usr/bin/python3 run_inference.py --select Org --END 2 --model_name IV3
+time /usr/bin/python3 run_inference.py --select Org --END 2 --model_name IV4
+time /usr/bin/python3 run_inference.py --select Org --END 2 --model_name MobileNet
+time /usr/bin/python3 run_inference.py --select Org --END 2 --model_name MobileNetV2
+time /usr/bin/python3 run_inference.py --select Org --END 2 --model_name AlexNet
+time /usr/bin/python3 run_inference.py --select Org --END 2 --model_name ResNet-V2-50
+time /usr/bin/python3 run_inference.py --select Org --END 2 --model_name ResNet-V2-101
+time /usr/bin/python3 run_inference.py --select Org --END 2 --model_name Pnasnet_Large
